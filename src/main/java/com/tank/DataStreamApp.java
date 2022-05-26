e56af95689bea7bca90c5c9811d2f94d8db6bf4f
fuchun <fuchun198435@163.com>
2022-05-26 10:16:54 +0800

    [fuchun] #refactor refactor code

--- src/main/java/com/tank/DataStreamApp.java
+++ src/main/java/com/tank/DataStreamApp.java
@@ -1,25 +1,19 @@
 package com.tank;
 
 
-import cn.hutool.json.JSONUtil;
-import com.tank.model.DebeziumSourceModel;
-import com.ververica.cdc.connectors.mysql.MySqlSource;
+import com.ververica.cdc.connectors.mysql.source.MySqlSourceBuilder;
 import com.ververica.cdc.connectors.mysql.table.StartupOptions;
-import com.ververica.cdc.debezium.DebeziumSourceFunction;
 import com.ververica.cdc.debezium.JsonDebeziumDeserializationSchema;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.SneakyThrows;
 import lombok.experimental.Accessors;
 import lombok.val;
-import org.apache.flink.api.common.functions.FilterFunction;
-import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.annotation.JsonProperty;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 
 import java.time.LocalDateTime;
-import java.util.Locale;
 
 /**
  * @author tank
@@ -27,8 +21,10 @@ import java.util.Locale;
 public class DataStreamApp {
   @SneakyThrows
   public static void main(String[] args) {
-    DebeziumSourceFunction<String> source = MySqlSource.<String>builder()
+    val mySqlSourceBuilder = new MySqlSourceBuilder<String>();
+    val source = mySqlSourceBuilder
         .hostname("tank")
+        .includeSchemaChanges(true)
         .port(3306)
         .username("root")
         .password("123")
@@ -40,13 +36,10 @@ public class DataStreamApp {
 
 
     val env = StreamExecutionEnvironment.getExecutionEnvironment();
-    env.enableCheckpointing(3000).setParallelism(1);
 
-    val tEnv = StreamTableEnvironment.create(env);
-
-     env.addSource(source)
-        .map((MapFunction<String, DebeziumSourceModel>) json -> JSONUtil.toBean(json, DebeziumSourceModel.class))
-        .filter((FilterFunction<DebeziumSourceModel>) debeziumSourceModel -> !"d".equals(debeziumSourceModel.getOp().toLowerCase(Locale.ROOT)));
+    env.fromSource(source, WatermarkStrategy.noWatermarks(), "cdc-demo").setParallelism(1)
+        //.map((MapFunction<String, DebeziumSourceModel>) json -> JSONUtil.toBean(json, DebeziumSourceModel.class))
+        .print("console").setParallelism(1);
 
     env.execute("cdc-demo");
   }
@@ -73,35 +66,35 @@ public class DataStreamApp {
 
 /**
  * {
- *   "before": {
- *     "id": 1,
- *     "member_id": "jack",
- *     "score": 19985
- *   },
- *   "after": {
- *     "id": 1,
- *     "member_id": "jack",
- *     "score": 19984
- *   },
- *   "source": {
- *     "version": "1.5.4.Final",
- *     "connector": "mysql",
- *     "name": "mysql_binlog_source",
- *     "ts_ms": 1653498663000,
- *     "snapshot": "false",
- *     "db": "customer_db",
- *     "sequence": null,
- *     "table": "tab_member_score",
- *     "server_id": 1,
- *     "gtid": null,
- *     "file": "binlog.000045",
- *     "pos": 10191,
- *     "row": 0,
- *     "thread": null,
- *     "query": null
- *   },
- *   "op": "u",
- *   "ts_ms": 1653498663591,
- *   "transaction": null
+ * "before": {
+ * "id": 1,
+ * "member_id": "jack",
+ * "score": 19985
+ * },
+ * "after": {
+ * "id": 1,
+ * "member_id": "jack",
+ * "score": 19984
+ * },
+ * "source": {
+ * "version": "1.5.4.Final",
+ * "connector": "mysql",
+ * "name": "mysql_binlog_source",
+ * "ts_ms": 1653498663000,
+ * "snapshot": "false",
+ * "db": "customer_db",
+ * "sequence": null,
+ * "table": "tab_member_score",
+ * "server_id": 1,
+ * "gtid": null,
+ * "file": "binlog.000045",
+ * "pos": 10191,
+ * "row": 0,
+ * "thread": null,
+ * "query": null
+ * },
+ * "op": "u",
+ * "ts_ms": 1653498663591,
+ * "transaction": null
  * }
  */
