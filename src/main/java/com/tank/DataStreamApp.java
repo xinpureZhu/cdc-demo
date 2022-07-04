17ad16e991d59738da3d6f54b22abf9c8e936215
fuchun <fuchun198435@163.com>
2022-07-05 00:51:50 +0800

    [fuchun] #feat basic sink to mysql

--- src/main/java/com/tank/DataStreamApp.java
+++ src/main/java/com/tank/DataStreamApp.java
@@ -8,6 +8,9 @@ import lombok.val;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.EnvironmentSettings;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+import org.apache.flink.types.Row;
+
+import static org.apache.flink.table.api.Expressions.$;
 
 /**
  * @author tank
@@ -17,21 +20,31 @@ public class DataStreamApp {
   public static void main(@NonNull String[] args) {
 
     val env = StreamExecutionEnvironment.getExecutionEnvironment();
+    env.setParallelism(1);
     val settings = EnvironmentSettings.newInstance().useBlinkPlanner().inStreamingMode().build();
-    val tableEnvironment = StreamTableEnvironment.create(env, settings);
+    val tableEnv = StreamTableEnvironment.create(env, settings);
 
     val sourceTabMembersSql = StrUtil.format("create table  tab_members_v1 (`id` BIGINT primary key  NOT ENFORCED , name String ,birthDay String ) {}",
         withTableInfo("127.0.0.1", "k8s_demo_v1", "root", "123", "tab_members"));
 
-    tableEnvironment.executeSql(sourceTabMembersSql);
+    tableEnv.executeSql(sourceTabMembersSql);
 
-    val targetTabMembersSql = "create table tab_members_v2 (`id` BIGINT  primary key  NOT ENFORCED,name String ,birthDay String ) with ('connector' = 'print')";
-    tableEnvironment.executeSql(targetTabMembersSql);
+    val targetTabMembersSql = "create table tab_members_v2 (`id` BIGINT primary key  NOT ENFORCED , name String ,birthDay String ) WITH (" +
+        "    'connector' = 'jdbc',\n" +
+        "    'url' = 'jdbc:mysql://127.0.0.1:3306/k8s_demo_v2',\n" +
+        "    'username'= 'root'," +
+        "    'password'= '123'," +
+        "    'table-name' = 'tab_members'" +
+        ")";
 
-    val transformSql = "insert into tab_members_v2(id, name, birthDay) select id,name,birthDay from tab_members_v1";
+    tableEnv.executeSql(targetTabMembersSql);
 
-    val result = tableEnvironment.executeSql(transformSql);
-    result.print();
+
+    val result = tableEnv.sqlQuery("select id, name, DATE_FORMAT(birthDay, 'yyyy-MM-dd') as birthDay from tab_members_v1");
+    result.select($("id"), $("name"), $("birthDay")).executeInsert("tab_members_v2", false);
+
+    val retractStream = tableEnv.toRetractStream(result, Row.class);
+    retractStream.print();
 
     env.execute("cdc-demo");
   }
